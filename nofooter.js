7850f57adfa2a0299f3554bd79efd594613831b9
François <fxpar33@gmail.com>
2020-04-13 21:18:06 +0200

    Add files via upload

--- nofooter.js
+++ nofooter.js
@@ -1,2 +1,12 @@
-//document.body.style.border = "5px solid green";
+document.body.style.border = "5px solid green";
+console.log("********Hello Footer:**********"+localStorage.getItem("footer"));
+
+chrome.storage.sync.get(['header', 'footer'], function(items) {
+      console.log('Settings retrievedFOOTER:'+items.footer);
+	  console.log('Settings retrievedHEADER:'+ items.header);
+    });
+
+if (localStorage.getItem("footer") === "true"){
+	console.log("footer="+localStorage.getItem("footer", footer));
 document.getElementById("page-footer").style.display = "none";
+}
